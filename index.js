7c620b2707a0ac8d58bc782a229d67fe9a0de835
Benjamin Yee <benyee1209@gmail.com>
2024-10-23 13:58:40 -0400

    s
    s

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function factorial(n){
+/*function factorial(n){
 //guard clause
     if (n < 1){
         return;
@@ -21,4 +21,26 @@ function newFactorial(n) {
     } else {
         return;
     }
+} */
+
+/*function checkSpaces(n, yesterday, today){
+    let count = 0;
+    for (let i = 0; i < n; i++){
+        if(yesterday[i] === `C && today`[i] === "C"){
+            count++;
+        }
+
+    }
+    return count;
 }
+
+console.log(checkSpaces(5, "CC..C", ".CC.."));*/
+
+
+function dtb(){
+    
+}
+
+
+
+
